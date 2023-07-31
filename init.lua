c27d04cdc37d8ee603646e4d220202984d286de3
sebasruiz09 <sebasruiz909@gmail.com>
2023-07-31 17:56:19 -0500

    FIX : solve errors in checkhealth

--- init.lua
+++ init.lua
@@ -1,11 +1,15 @@
 require "config"
-vim.opt.fillchars = { 
-    vert = "▕", -- alternatives │
-    fold = " ",
-    eob = " ", -- suppress ~ at EndOfBuffer
-    diff = "╱", -- alternatives = ⣿ ░ ─
-    msgsep = "‾",
-    foldopen = "▾",
-    foldsep = "│",
-    foldclose = "▸",
+vim.opt.fillchars = {
+  vert = "▕", -- alternatives │
+  fold = " ",
+  eob = " ", -- suppress ~ at EndOfBuffer
+  diff = "╱", -- alternatives = ⣿ ░ ─
+  msgsep = "‾",
+  foldopen = "▾",
+  foldsep = "│",
+  foldclose = "▸",
 }
+
+vim.cmd("verbose 15")
+vim.g.loaded_perl_provider = 0;
+vim.opt.clipboard:append { "unnamedplus" }
