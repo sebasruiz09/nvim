dc8efc38d2f279df1dab44e5ffc5990e997adbd8
sebasruiz09 <sebasruiz909@gmail.com>
2023-07-31 20:34:13 -0500

    FIX : lsp work on all servers

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,29 +1,28 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 
 require("lazy").setup({
-	spec = {
-		{ import="plugins"},
-	},
-	checker = { enable = true , notify = false },
-	dev = {
-		paths = {
-			"~/plugins,",
-			"~/plugins/telescope"
-		},
-		fallback = true,
-		patterns = {"sebasruiz"},
-	},
+  spec = {
+    { import = "plugins" },
+  },
+  checker = { enable = true, notify = false },
+  dev = {
+    paths = {
+      "~/plugins,",
+      "~/plugins/telescope"
+    },
+    fallback = true,
+    patterns = { "sebasruiz" },
+  },
 })
-
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,5 +1,6 @@
 return {
   "williamboman/mason.nvim",
+  priority = 1000,
   dependencies = {
     { "williamboman/mason-lspconfig.nvim" },
     { "neovim/nvim-lspconfig" }
@@ -7,9 +8,30 @@ return {
   config = function()
     require("mason").setup()
 
+    -- angular-language-server path
+    local ngserver = "/home/sebas/.nvm/versions/node/v18.17.0/bin/ngserver"
+    local project_library_path = "/home/sebas/.nvm/versions/node/v18.17.0/lib/"
+    local cmd = { ngserver, "--stdio", "--tsProbeLocations", project_library_path, "--ngProbeLocations",
+      project_library_path }
+
+    require 'lspconfig'.angularls.setup {
+      cmd = cmd,
+      on_new_config = function(new_config, new_root_dir)
+        new_config.cmd = cmd
+      end,
+    }
+    local configs = {
+      angularls = {
+        cmd = cmd,
+        on_new_config = function(new_config, new_root_dir)
+          new_config.cmd = cmd
+        end
+      }
+    }
     local startLsp = function(lspname)
       local lspconfig = require 'lspconfig'
-      lspconfig[lspname].setup {}
+      local settings = configs[lspname] or {}
+      lspconfig[lspname].setup(settings)
     end
 
     vim.api.nvim_create_user_command(
