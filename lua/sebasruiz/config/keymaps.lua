4bf366961c7644eacbf3dd13fafc9d90639f36f1
sebasruiz09 <sebasruiz909@gmail.com>
2024-03-09 10:56:25 -0500

    ADD : vim.lsp.buf keymaps

--- lua/sebasruiz/config/keymaps.lua
+++ lua/sebasruiz/config/keymaps.lua
@@ -2,9 +2,10 @@ local opts = { noremap = true, silent = true }
 
 local keymap = vim.api.nvim_set_keymap
 
-keymap("", "<Space>", "<Nop>", opts)
+vim.keymap.set("v", "K", ":m '<-2<cr>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<cr>gv=gv")
 
-keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+keymap("", "<Space>", "<Nop>", opts)
 
 keymap("n", "<C-Up>", ":resize +2<CR>", opts)
 keymap("n", "<C-Down>", ":resize -2<CR>", opts)
@@ -17,5 +18,10 @@ keymap("n", "<leader>bd", ":bd!<CR>", opts)
 
 keymap("t", "<C-Del>", "<C-\\><C-n>", opts)
 
-keymap("n", "<leader>vn", "<cmd>lua vim.diagnostic.goto_next()<cr>", { desc = "Goes to next diagnostic error" })
-keymap("n", "<leader>vp", "<cmd>lua vim.diagnostic.goto_prev()<cr>", { desc = "Goes to prev diagnostic error" })
+keymap("n", "<leader>vn", "<cmd>lua vim.diagnostic.goto_next()<CR>", { desc = "Goes to next diagnostic error" })
+keymap("n", "<leader>vp", "<cmd>lua vim.diagnostic.goto_prev()<CR>", { desc = "Goes to prev diagnostic error" })
+keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+keymap("i", "<C-h>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+keymap("n", "<leader>vd", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+keymap("n", "<leader>vca", "<md>lua vim.lsp.buf.code_action()<CR>", opts)
